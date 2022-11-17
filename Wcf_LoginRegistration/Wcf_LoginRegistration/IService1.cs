f7e213453d46f82c08849dc41b21f088c79a15eb
zoli <farkasz@kkszki.hu>
2022-11-17 09:37:21 +0100

    Backend szerkezet kialakitva

---
+++ Wcf_LoginRegistration/Wcf_LoginRegistration/IService1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Wcf_LoginRegistration
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+
+        [WebInvoke(Method = "*",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "login")]
+        string login(User user);
+
+      
+    }
+
+   
+}
